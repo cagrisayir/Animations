e829ecba70a4b1c47c3ef9af7b5d76b81df398db
Omer Cagri Sayir <omercagris@gmail.com>
2023-12-26 09:47:34 +0300

    animation

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,13 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 1.0
+
     var body: some View {
         Button("Tap me") {
-            
+            animationAmount += 1
         }.padding(50)
             .background(.red)
             .foregroundColor(.white)
             .clipShape(Circle())
+            .scaleEffect(animationAmount)
+//            .blur(radius: (animationAmount - 1) * 3)
+//            .animation(.interpolatingSpring(stiffness: 50, damping: 1), value: animationAmount)
+//            .animation(.easeInOut(duration: 2).delay(1), value: animationAmount)
+//            .animation(
+//                .easeInOut(duration: 1)
+//                    .repeatCount(3, autoreverses: true),
+//                value: animationAmount
+//            )
+            .animation(
+                .easeInOut(duration: 1)
+                    .repeatForever(autoreverses: true), value: animationAmount
+            )
     }
 }
 
